c28d4170e0c970ef583ecbab9d410208b9b31740
Mahmut <mahmutla33@gmail.com>
2019-11-09 22:34:21 +0300

    origin

--- src/com/company/Multithread.java
+++ src/com/company/Multithread.java
@@ -1,36 +1,40 @@
 package com.company;
 // Java code for thread creation by extending
 // the Thread class
-class MultithreadingDemo extends Thread
-{
-    public void run()
-    {
-        try
-        {
-            // Displaying the thread that is running
-            System.out.println ("Thread " +
-                    Thread.currentThread().getId() +
-                    " is running");
+class MyThread implements Runnable {
+    String name;
+    Thread t;
+    MyThread (String thread){
+        name = thread;
+        t = new Thread(this, name);
+        System.out.println("New thread: " + t);
+        t.start();
+    }
 
+
+    public void run() {
+        try {
+            for(int i = 5; i > 0; i--) {
+                System.out.println(name + ": " + i);
+                Thread.sleep(1000);
+            }
+        }catch (InterruptedException e) {
+            System.out.println(name + "Interrupted");
         }
-        catch (Exception e)
-        {
-            // Throwing an exception
-            System.out.println ("Exception is caught");
-        }
+        System.out.println(name + " exiting.");
     }
 }
 
-// Main Class
-public class Multithread
-{
-    public static void main(String[] args)
-    {
-        int n = 2; // Number of threads
-        for (int i=0; i<n; i++)
-        {
-            MultithreadingDemo object = new MultithreadingDemo();
-            object.start();
+public class Multithread {
+    public static void main(String args[]) {
+        new MyThread("One");
+        new MyThread("Two");
+        new MyThread("Three");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            System.out.println("Main thread Interrupted");
         }
+        System.out.println("Main thread exiting.");
     }
 }
